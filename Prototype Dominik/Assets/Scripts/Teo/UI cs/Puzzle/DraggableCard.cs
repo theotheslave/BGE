eff8cc65fdef4c1a0f44f10c5b09cc00c6784165
theotheslave <teodorsim@abv.bg>
2025-05-15 11:08:46 +0200

    issue withing issues ( you can call it issueception)

--- Prototype Dominik/Assets/Scripts/Teo/UI cs/Puzzle/DraggableCard.cs
+++ Prototype Dominik/Assets/Scripts/Teo/UI cs/Puzzle/DraggableCard.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine.EventSystems;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DraggableCard : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
@@ -7,17 +8,22 @@ public class DraggableCard : MonoBehaviour, IBeginDragHandler, IDragHandler, IEn
     private RectTransform rectTransform;
     private Transform originalParent;
     private Vector3 originalPosition;
-
+    private LayoutElement layoutElement;
     void Awake()
     {
         canvasGroup = GetComponent<CanvasGroup>() ?? gameObject.AddComponent<CanvasGroup>();
         rectTransform = GetComponent<RectTransform>();
+
+        layoutElement = GetComponent<LayoutElement>();
     }
 
     public void OnBeginDrag(PointerEventData eventData)
     {
         originalParent = transform.parent;
         originalPosition = rectTransform.anchoredPosition;
+
+        if (layoutElement != null)
+            layoutElement.ignoreLayout = true;
 
         var dragLayer = GameObject.Find("DragLayer");
         if (dragLayer)
@@ -48,5 +54,8 @@ public class DraggableCard : MonoBehaviour, IBeginDragHandler, IDragHandler, IEn
     {
         transform.SetParent(originalParent, false);
         rectTransform.anchoredPosition = originalPosition;
+
+        if (layoutElement != null)
+            layoutElement.ignoreLayout = false;
     }
 }
