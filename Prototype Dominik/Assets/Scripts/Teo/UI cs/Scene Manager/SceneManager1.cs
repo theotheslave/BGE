68229a7820a7e764babd8b3af71895203c4e158d
theotheslave <teodorsim@abv.bg>
2025-06-10 06:15:40 +0200

    idk more tweaks

---
+++ Prototype Dominik/Assets/Scripts/Teo/UI cs/Scene Manager/SceneManager1.cs
@@ -0,0 +1,80 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+[RequireComponent(typeof(Collider))]
+public class SceneManagerDoor1 : MonoBehaviour
+{
+    [SerializeField] private int targetSceneIndex = 0;
+    
+    [SerializeField] private Texture2D unlockedCursor;
+    [SerializeField] private Vector2 hotspot = Vector2.zero;
+
+    private Outline outline;
+    private static Transform currentHighlight;
+
+    private bool isUnlocked = false;
+
+    void Awake()
+    {
+
+        outline = GetComponent<Outline>();
+        if (outline == null)
+        {
+            outline = gameObject.AddComponent<Outline>();
+            outline.OutlineColor = Color.cyan;
+            outline.OutlineWidth = 6f;
+        }
+
+        if (outline != null)
+            outline.enabled = false;
+
+    }
+
+    void Update()
+    {
+        bool hoveredThisFrame = false;
+
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (!EventSystem.current.IsPointerOverGameObject() && Physics.Raycast(ray, out RaycastHit hit))
+        {
+            if (hit.transform == transform)
+            {
+                hoveredThisFrame = true;
+
+                
+
+                if (currentHighlight != transform)
+                {
+                    if (currentHighlight != null)
+                    {
+                        Outline prevOutline = currentHighlight.GetComponent<Outline>();
+                        if (prevOutline != null) prevOutline.enabled = false;
+                    }
+
+                    currentHighlight = transform;
+                }
+
+                if (outline != null) outline.enabled = true;
+            }
+        }
+
+        if (!hoveredThisFrame && currentHighlight == transform)
+        {
+            if (outline != null) outline.enabled = false;
+            Cursor.SetCursor(null, Vector2.zero, CursorMode.Auto);
+            currentHighlight = null;
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        if (EventSystem.current.IsPointerOverGameObject()) return;
+     
+
+        FadeToBlack.Instance.FadeToScene(targetSceneIndex);
+
+    }
+
+  
+}
