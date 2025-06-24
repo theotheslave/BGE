0f50ea6dc8ad1f62c4214421c42753c8e1c9a2d6
theotheslave <teodorsim@abv.bg>
2025-06-24 03:09:33 +0200

    EVENT SYSTEM 🚑

--- Prototype Dominik/Assets/Scripts/Teo/UI cs/Camera Control/PuzzleControlRoom2.cs
+++ Prototype Dominik/Assets/Scripts/Teo/UI cs/Camera Control/PuzzleControlRoom2.cs
@@ -1,66 +1,66 @@
-using UnityEngine;
+//using UnityEngine;
 
-public class PuzzleControlRoom2 : MonoBehaviour
-{
-    [Header("Puzzle Logic")]
-    public IsochoricMinigame isochoricPuzzle;
-    public IsothermalMinigameFinal isothermalPuzzle;
-    public SceneManagerDoor finalDoor;
+//public class PuzzleControlRoom2 : MonoBehaviour
+//{
+//    [Header("Puzzle Logic")]
+//    public IsochoricMinigame isochoricPuzzle;
+//    public IsothermalMinigameFinal isothermalPuzzle;
+//    public SceneManagerDoor finalDoor;
 
-    [Header("Puzzle Panels")]
-    [SerializeField] private GameObject isochoricPanel;
-    [SerializeField] private GameObject isothermalPanel;
+//    [Header("Puzzle Panels")]
+//    [SerializeField] private GameObject isochoricPanel;
+//    [SerializeField] private GameObject isothermalPanel;
 
-    private bool isochoricDone = false;
-    private bool isothermalDone = false;
+//    private bool isochoricDone = false;
+//    private bool isothermalDone = false;
 
-    void Start()
-    {
-        if (isochoricPuzzle != null)
-            isochoricPuzzle.OnPuzzleComplete += HandleIsochoricComplete;
+//    void Start()
+//    {
+//        if (isochoricPuzzle != null)
+//            isochoricPuzzle.OnPuzzleComplete += HandleIsochoricComplete;
 
-        if (isothermalPuzzle != null)
-            isothermalPuzzle.OnPuzzleComplete += HandleIsothermalComplete;
+//        if (isothermalPuzzle != null)
+//            isothermalPuzzle.OnPuzzleComplete += HandleIsothermalComplete;
 
-        isochoricPanel?.SetActive(false);
-        isothermalPanel?.SetActive(false);
-    }
+//        isochoricPanel?.SetActive(false);
+//        isothermalPanel?.SetActive(false);
+//    }
 
-    void HandleIsochoricComplete()
-    {
-        isochoricDone = true;
-        Debug.Log("Isochoric complete");
-        CameraMovement.Instance?.ReturnToStart();
-        CameraMovement.Instance?.FocusGoggles();
-    }
+//    void HandleIsochoricComplete()
+//    {
+//        isochoricDone = true;
+//        Debug.Log("Isochoric complete");
+//        CameraMovement.Instance?.ReturnToStart();
+//        CameraMovement.Instance?.FocusGoggles();
+//    }
 
-    void HandleIsothermalComplete()
-    {
-        isothermalDone = true;
-        Debug.Log("Isothermal complete");
-        CameraMovement.Instance?.ReturnToStart();
-        if (finalDoor != null)
-            finalDoor.UnlockDoor();
-    }
+//    void HandleIsothermalComplete()
+//    {
+//        isothermalDone = true;
+//        Debug.Log("Isothermal complete");
+//        CameraMovement.Instance?.ReturnToStart();
+//        if (finalDoor != null)
+//            finalDoor.UnlockDoor();
+//    }
 
-    public void ActivatePuzzle(SelectableObject.PuzzleType type)
-    {
-        if (type == SelectableObject.PuzzleType.Isochoric)
-        {
-            isochoricPanel?.SetActive(true);
-            isothermalPanel?.SetActive(false);
-        }
-        else if (type == SelectableObject.PuzzleType.Isothermal)
-        {
-            isothermalPanel?.SetActive(true);
-            isochoricPanel?.SetActive(false);
-        }
-    }
+//    public void ActivatePuzzle(SelectableObject.PuzzleType type)
+//    {
+//        if (type == SelectableObject.PuzzleType.Isochoric)
+//        {
+//            isochoricPanel?.SetActive(true);
+//            isothermalPanel?.SetActive(false);
+//        }
+//        else if (type == SelectableObject.PuzzleType.Isothermal)
+//        {
+//            isothermalPanel?.SetActive(true);
+//            isochoricPanel?.SetActive(false);
+//        }
+//    }
 
-    public void ReturnToHub()
-    {
-        isochoricPanel?.SetActive(false);
-        isothermalPanel?.SetActive(false);
-        CameraMovement.Instance?.ReturnToStart();
-    }
-}
+//    public void ReturnToHub()
+//    {
+//        isochoricPanel?.SetActive(false);
+//        isothermalPanel?.SetActive(false);
+//        CameraMovement.Instance?.ReturnToStart();
+//    }
+//}
