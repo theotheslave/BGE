e4976618fc383f6a68c86e549652e4dfeea42d9d
theotheslave <teodorsim@abv.bg>
2025-06-24 02:01:36 +0200

    Hopefully I didnt break it

--- Prototype Dominik/Assets/Scripts/Teo/UI cs/Camera Control/PuzzleControlRoom2.cs
+++ Prototype Dominik/Assets/Scripts/Teo/UI cs/Camera Control/PuzzleControlRoom2.cs
@@ -2,36 +2,65 @@ using UnityEngine;
 
 public class PuzzleControlRoom2 : MonoBehaviour
 {
+    [Header("Puzzle Logic")]
     public IsochoricMinigame isochoricPuzzle;
     public IsothermalMinigameFinal isothermalPuzzle;
-    public SceneManagerDoor finalDoor; // Optional
+    public SceneManagerDoor finalDoor;
+
+    [Header("Puzzle Panels")]
+    [SerializeField] private GameObject isochoricPanel;
+    [SerializeField] private GameObject isothermalPanel;
 
     private bool isochoricDone = false;
     private bool isothermalDone = false;
 
     void Start()
     {
-        isochoricPuzzle.OnPuzzleComplete += HandleIsochoricComplete;
-        isothermalPuzzle.OnPuzzleComplete += HandleIsothermalComplete;
+        if (isochoricPuzzle != null)
+            isochoricPuzzle.OnPuzzleComplete += HandleIsochoricComplete;
+
+        if (isothermalPuzzle != null)
+            isothermalPuzzle.OnPuzzleComplete += HandleIsothermalComplete;
+
+        isochoricPanel?.SetActive(false);
+        isothermalPanel?.SetActive(false);
     }
 
     void HandleIsochoricComplete()
     {
         isochoricDone = true;
         Debug.Log("Isochoric complete");
-
-        CameraMovement.Instance?.ReturnToStart(); 
-        CameraMovement.Instance?.FocusGoggles(); // focus on next puzzle
+        CameraMovement.Instance?.ReturnToStart();
+        CameraMovement.Instance?.FocusGoggles();
     }
 
     void HandleIsothermalComplete()
     {
         isothermalDone = true;
         Debug.Log("Isothermal complete");
-
-        CameraMovement.Instance?.ReturnToStart(); // or focus on a door, ending area, etc.
-
+        CameraMovement.Instance?.ReturnToStart();
         if (finalDoor != null)
-            finalDoor.UnlockDoor(); // Allow scene transition
+            finalDoor.UnlockDoor();
+    }
+
+    public void ActivatePuzzle(SelectableObject.PuzzleType type)
+    {
+        if (type == SelectableObject.PuzzleType.Isochoric)
+        {
+            isochoricPanel?.SetActive(true);
+            isothermalPanel?.SetActive(false);
+        }
+        else if (type == SelectableObject.PuzzleType.Isothermal)
+        {
+            isothermalPanel?.SetActive(true);
+            isochoricPanel?.SetActive(false);
+        }
+    }
+
+    public void ReturnToHub()
+    {
+        isochoricPanel?.SetActive(false);
+        isothermalPanel?.SetActive(false);
+        CameraMovement.Instance?.ReturnToStart();
     }
 }
